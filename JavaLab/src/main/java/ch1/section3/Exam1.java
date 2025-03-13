3e899d9877718873884c84970ac9b365e38e431e
kkangs <kkang104@daum.net>
2025-03-13 17:35:48 +0900

    .

---
+++ JavaLab/src/main/java/ch1/section3/Exam1.java
@@ -0,0 +1,18 @@
+package ch1.section3;
+
+//하나의 자바 파일에 필요에 의해 여러 클래스를 선언할 수 있다..
+//class Some1 { }
+//class Some2 { }
+
+//public 으로 클래스가 선언될 수 있지만.. public 으로 선언된 클래스는
+//파일명과 대소문자가 동일해야 한다..
+//class Some1 { }
+//public class Some2 { }//error...
+
+//하나의 자바 파일에 여러개의 클래스가 선언할 수 있지만..
+//public 으로 선언하는 클래스는 하나만 허용..
+//public class Exam1 { }
+//public class Some2 { }//error...
+
+public class Exam1 { }
+class Some2 { }
