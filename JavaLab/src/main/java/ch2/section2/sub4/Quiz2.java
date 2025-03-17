a33b6fec60774a72f5d0bd9664e2073e0c1601e7
kkangs <kkang104@daum.net>
2025-03-17 15:33:55 +0900

    .

--- JavaLab/src/main/java/ch2/section2/sub4/Quiz2.java
+++ JavaLab/src/main/java/ch2/section2/sub4/Quiz2.java
@@ -4,28 +4,28 @@ public class Quiz2 {
     public static void main(String[] args) {
         int num = 12;
 
-        switch (num) {
-
-            case 3, 4, 5:
-                System.out.println("봄입니다.");
-                break;
-
-            case 6, 7, 8:
-                System.out.println("여름입니다.");
-                break;
-
-            case 9, 10, 11:
-                System.out.println("가을입니다.");
-                break;
-
-            case 12, 1 , 2:
-                System.out.println("겨울입니다.");
-                break;
-
-            default:
-                System.out.println("잘못된 입력입니다.");
-                break;
-        }
+//        switch (num) {
+//
+//            case 3, 4, 5:
+//                System.out.println("봄입니다.");
+//                break;
+//
+//            case 6, 7, 8:
+//                System.out.println("여름입니다.");
+//                break;
+//
+//            case 9, 10, 11:
+//                System.out.println("가을입니다.");
+//                break;
+//
+//            case 12, 1 , 2:
+//                System.out.println("겨울입니다.");
+//                break;
+//
+//            default:
+//                System.out.println("잘못된 입력입니다.");
+//                break;
+//        }
 
     }
 }
