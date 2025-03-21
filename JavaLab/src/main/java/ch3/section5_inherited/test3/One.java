6638be751067d7c55eb0b3e5c90763b5f0e53e55
kkangs <kkang104@daum.net>
2025-03-21 13:07:28 +0900

    .

---
+++ JavaLab/src/main/java/ch3/section5_inherited/test3/One.java
@@ -0,0 +1,10 @@
+package ch3.section5_inherited.test3;
+
+//Exam3 와 동일 패키지의 클래스..
+
+public class One {
+    private int privateData = 0;
+    protected int protectedData = 0;
+    int defaultData = 0;
+    public int publicData = 0;
+}
---
+++ JavaLab/src/main/java/ch3/section5_inherited/test3/sub/Super.java
@@ -0,0 +1,8 @@
+package ch3.section5_inherited.test3.sub;
+
+public class Super {
+    private int privateSuperData = 0;
+    protected int protectedSuperData = 0;
+    int defaultSuperData = 0;
+    public int publicSuperData = 0;
+}
