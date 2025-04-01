afc666d54504899432d6a2616bd5bf16829426ee
kkangs <kkang104@daum.net>
2025-04-01 10:07:51 +0900

    .

--- AndroidLab/ch3/src/main/java/com/example/ch3/section5/Test5_1Activity.java
+++ AndroidLab/ch3/src/main/java/com/example/ch3/section5/Test5_1Activity.java
@@ -1,8 +1,6 @@
-package com.example.ch2;
+package com.example.ch3.section5;
 
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.widget.Chronometer;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.ch2.databinding.ActivityMainBinding;
+import com.example.ch3.R;
 
-import java.time.temporal.ChronoField;
-
-public class MainActivity extends AppCompatActivity {
+public class Test5_1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_test51);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 }
