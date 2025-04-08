9e7825259d3bce45ce85d9c8f0be4fee3f775528
kkangs <kkang104@daum.net>
2025-04-08 11:04:23 +0900

    .

--- AndroidLab/ch7/src/main/java/com/example/ch7/section1/Test1_1Activity.java
+++ AndroidLab/ch7/src/main/java/com/example/ch7/section1/Test1_1Activity.java
@@ -1,10 +1,15 @@
 package com.example.ch7.section1;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -28,14 +33,41 @@ public class Test1_1Activity extends AppCompatActivity {
             return insets;
         });
 
+        //퍼미션 조정 다이얼로그를 띄우고.. 닫혔을때 사후 처리 콜백이 등록되어 있는 곳..
+        ActivityResultLauncher<String> launcher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {//callback.. 매개변수 true - 허락, false -> 거부
+                    if(isGranted){
+                        Intent intent = new Intent("CH7_ACTION_TWO");
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(this, "permission denied..", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+        );
+
         binding.button1.setOnClickListener(view -> {
+            //normal protectionLevel 로 선언 되어 있으면.. AndroidManifest.xml 에 <uses-permission> 선언으로 끝
             Intent intent = new Intent("CH7_ACTION_ONE");
             startActivity(intent);
         });
 
         binding.button2.setOnClickListener(view -> {
-            Intent intent = new Intent("CH7_ACTION_TWO");
-            startActivity(intent);
+            //dangerous 로 선언되어 있다면.. 퍼미션 상태 체크하고 거부 상태라면 조정 다이얼로그 띄워야 한다..
+            //현재의 퍼미션 상태 체크..
+            if(ContextCompat.checkSelfPermission(this,
+                    "com.example.ch7_outer.TWO_PERMISSION") == PackageManager.PERMISSION_GRANTED){
+                //허락
+                Intent intent = new Intent("CH7_ACTION_TWO");
+                startActivity(intent);
+            }else {
+                //거부
+                //퍼미션 조정 다이얼로그를 띄운다..
+                launcher.launch("com.example.ch7_outer.TWO_PERMISSION");
+            }
+
+
         });
     }
 }
