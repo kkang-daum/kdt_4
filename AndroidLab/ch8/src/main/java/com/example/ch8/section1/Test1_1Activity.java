f6fd0e1b07f0a92e6a9293c5e257a20015e9b5c3
kkangs <kkang104@daum.net>
2025-04-08 16:13:20 +0900

    .

---
+++ AndroidLab/ch8/src/main/java/com/example/ch8/section1/Test1_1Activity.java
@@ -0,0 +1,40 @@
+package com.example.ch8.section1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ch8.R;
+import com.example.ch8.databinding.ActivityTest11Binding;
+
+public class Test1_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        ActivityTest11Binding binding = ActivityTest11Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Sub1Activity.class);
+            startActivity(intent);
+        });
+        binding.button2.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Sub2Activity.class);
+            startActivity(intent);
+        });
+    }
+}
