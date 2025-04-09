1b9c9002022dd58a45857897340840d756fa3fed
kkangs <kkang104@daum.net>
2025-04-09 14:49:41 +0900

    .

--- AndroidLab/ch9/src/main/java/com/example/ch9/section1/Test1_1Activity.java
+++ AndroidLab/ch9/src/main/java/com/example/ch9/section1/Test1_1Activity.java
@@ -1,6 +1,7 @@
 package com.example.ch9.section1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -31,6 +32,28 @@ public class Test1_1Activity extends AppCompatActivity {
         binding.button1.setOnClickListener(view -> {
             //Sub1Activity... 명시적...
             Intent intent = new Intent(this, Sub1Activity.class);
+            startActivity(intent);
+        });
+
+        binding.button2.setOnClickListener(view -> {
+            //Sub2Activity... 암시적....
+            Intent intent = new Intent();
+            intent.setAction("ACTION_SUB2");
+            //android.intent.category.DEFAULT category 에 한해서는.. 정보를 주지 않아도 된다..
+            //기본이 android.intent.category.DEFAULT category 에서 뒤지게 되어 있다..
+            startActivity(intent);
+        });
+
+        binding.button3.setOnClickListener(view -> {
+            //Sub3Activity를 암시적으로... category 정보 지정해서..
+            Intent intent = new Intent();
+            intent.setAction("ACTION_SUB3");
+            intent.addCategory("com.example.ch9.CATEGORY_SUB3");
+            startActivity(intent);
+        });
+        binding.button4.setOnClickListener(view -> {
+            //Sub4Activity를 암시적으로 , data 정보 설정..
+            Intent intent = new Intent("ACTION_SUB4", Uri.parse("http://www.naver.com"));
             startActivity(intent);
         });
     }
