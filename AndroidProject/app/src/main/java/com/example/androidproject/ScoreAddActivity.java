18b9b81aa363be3c2af8ce92ca8a15da66a1aefa
kkangs <kkang104@daum.net>
2025-04-14 11:49:04 +0900

    .

--- AndroidProject/app/src/main/java/com/example/androidproject/ScoreAddActivity.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/ScoreAddActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidproject;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,67 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ScoreAddActivity extends AppCompatActivity {
+import com.example.androidproject.databinding.ActivityScoreAddBinding;
+import com.example.androidproject.db.DBHelper;
+
+public class ScoreAddActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityScoreAddBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_score_add);
+
+        binding = ActivityScoreAddBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.key0.setOnClickListener(this);
+        binding.key1.setOnClickListener(this);
+        binding.key2.setOnClickListener(this);
+        binding.key3.setOnClickListener(this);
+        binding.key4.setOnClickListener(this);
+        binding.key5.setOnClickListener(this);
+        binding.key6.setOnClickListener(this);
+        binding.key7.setOnClickListener(this);
+        binding.key8.setOnClickListener(this);
+        binding.key9.setOnClickListener(this);
+        binding.keyBack.setOnClickListener(this);
+        binding.keyAdd.setOnClickListener(this);
+    }
+
+    //여러 뷰의 이벤트를 하나의 함수에서 모두 처리.. 매개변수가 현재 이벤트가 발생한 객체..
+    //매개변수로 식별해서....
+    @Override
+    public void onClick(View v) {
+        if(v == binding.keyAdd){
+            //DetailActivity 에 의해 이 액티비티가 실행되었을 것이다..
+            //DetailActivity 가 전달해준.. 학생 id 값을 추출한다..
+            Intent intent = getIntent();
+            int id = intent.getIntExtra("id", 0);
+
+            long date = System.currentTimeMillis();
+
+            String score = binding.keyEdit.getText().toString();
+
+            DBHelper helper = new DBHelper(this);
+            SQLiteDatabase db = helper.getWritableDatabase();
+            db.execSQL("insert into tb_score (student_id, date, score) values (?,?,?)",
+                    new String[]{String.valueOf(id), String.valueOf(date), score});
+            db.close();
+
+            //화면은?????
+            //CRUD 의 create 화면이다.. 목록 화면으로 자동 전환..DetailActivity... 데이터 넘기면서..
+            intent.putExtra("score", score);
+            intent.putExtra("date", date);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }
