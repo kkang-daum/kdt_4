ccb1b0eab185549e19fe09eb687a30e19643a09f
kkangs <kkang104@daum.net>
2025-04-23 13:05:54 +0900

    .

---
+++ KotlinLab/KotlinLab/src/main/kotlin/ch2/section4_control/q2/Quiz1.kt
@@ -0,0 +1,17 @@
+package likelion.study.ch2.section4_control
+
+class Quiz1 {
+    fun calcBonus(salary: Int, bonusRate: Double): String {
+        return if (salary <= 0 || bonusRate <= 0) {
+            "잘못된 데이터를 입력했습니다."
+        } else {
+            val bonus = (salary * bonusRate).toInt()
+            "지급받을 보너스는 ${bonus}원 입니다."
+        }
+    }
+}
+
+fun main() {
+    val obj = Quiz1()
+    println(obj.calcBonus(1000, 0.25))
+}
