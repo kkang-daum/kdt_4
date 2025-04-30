81304139429b967d6b2f98a6eae0cdf0fcceab32
kkangs <kkang104@daum.net>
2025-04-30 10:06:42 +0900

    .

---
+++ KotlinLab/KotlinLab/src/main/java/ch3/A.java
@@ -0,0 +1,14 @@
+package ch3;
+
+public class A {
+//    int a = 0;
+//    static int b = 0;
+//    void some(){
+//        a++;
+//        b++;
+//    }
+//    static void some1(){
+//        b++;
+//        a++;
+//    }
+}
