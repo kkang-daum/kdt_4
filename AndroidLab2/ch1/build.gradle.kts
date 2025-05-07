0acc7454ed856eb31b140c678dd25c186c311009
kkangs <kkang104@daum.net>
2025-05-08 08:00:40 +0900

    .

--- AndroidLab2/ch1/build.gradle.kts
+++ AndroidLab2/ch1/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
 }
