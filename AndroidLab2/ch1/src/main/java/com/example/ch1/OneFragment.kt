f67fee0f57f20a03aa6812a58da15ccd82076081
kkangs <kkang104@daum.net>
2025-05-07 13:09:44 +0900

    .

---
+++ AndroidLab2/ch1/src/main/java/com/example/ch1/OneFragment.kt
@@ -0,0 +1,23 @@
+package com.example.ch1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ch1.databinding.FragmentOneBinding
+
+class OneFragment: Fragment() {
+    //자동 호출되는 라이프사이클 함수..
+    //Fragment의 화면 구성을 목적으로 호출...
+    //이 함수에서 리턴 시키는 뷰 객체가 Fragment 화면이다..
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
