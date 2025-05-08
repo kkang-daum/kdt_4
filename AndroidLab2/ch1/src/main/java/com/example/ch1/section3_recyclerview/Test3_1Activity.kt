fbead7d6fba948e49b527328aa09bfd3b66fdafc
kkangs <kkang104@daum.net>
2025-05-08 11:00:48 +0900

    .

--- AndroidLab2/ch1/src/main/java/com/example/ch1/section3_recyclerview/Test3_1Activity.kt
+++ AndroidLab2/ch1/src/main/java/com/example/ch1/section3_recyclerview/Test3_1Activity.kt
@@ -1,10 +1,17 @@
 package com.example.ch1.section3_recyclerview
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -85,3 +92,63 @@ class MyAdapter(val data: MutableList<String>): RecyclerView.Adapter<MyViewHolde
         binding.itemData.text = data[position]
     }
 }
+
+class MyDecoration(val context: Context): RecyclerView.ItemDecoration(){
+    //항목이 찍히기 전에 최초에 한번.. 바탕 드로잉이 필요할 때...
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.stadium),
+            0f, 0f, null)
+    }
+
+    //항목이 모두 찍힌 후 최후에 한번.. 항목 위에 추가 드로잉이 필요한 경우..
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+
+        //이미지를 recyclerview 의 center 위치에 그리고 싶다.. 좌표계산..
+        //뷰의 크기 계산..
+        val width = parent.width
+        val height = parent.height
+
+        //드로잉 하고자 하는 이미지 사이즈..
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources,
+            R.drawable.kbo, null)
+        val drWidth = dr?.intrinsicWidth
+        val drHeight = dr?.intrinsicHeight
+
+        //이미지가 center에 그려지게.. 이미지가 그려질 left, top 좌표값 계산..
+        val left = width/2 - drWidth?.div(2) as Int
+        val top = height/2 - drHeight?.div(2) as Int
+
+        //이미지 그리기...
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            left.toFloat(), top.toFloat(), null)
+    }
+    //항목 하나당 한번씩 호출.. 항목에 추가 꾸미기 작업이 필요할 때...
+    //outRect : 항목이 출력되는 사각형 정보..
+    //view : 항목에 출력되는 뷰 객체..
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        //매개변수로 몇번째 항목을 꾸미기 위해서 호출된 것인지, 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
