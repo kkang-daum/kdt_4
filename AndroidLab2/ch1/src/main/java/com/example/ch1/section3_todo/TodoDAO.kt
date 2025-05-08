aceeba68510ed4f01fc7a35ef655b28949988d3e
kkangs <kkang104@daum.net>
2025-05-08 14:11:06 +0900

    .

--- AndroidLab2/ch1/src/main/java/com/example/ch1/section3_todo/TodoDAO.kt
+++ AndroidLab2/ch1/src/main/java/com/example/ch1/section3_todo/TodoDAO.kt
@@ -1,5 +1,13 @@
 package com.example.ch1.section3_todo
 
+import android.content.ContentValues
+import android.content.Context
+
 //DAO (Data Access Object) - dbms(select, insert, update 등... ) 를 위한 코드를 모아 놓은 곳..
-class TodoDAO {
+
+fun insertTodo(context: Context, values: ContentValues){
+    val helper = DBHelper(context)
+    val db = helper.writableDatabase
+    db.insert("tb_todo", null, values)
+    db.close()
 }
