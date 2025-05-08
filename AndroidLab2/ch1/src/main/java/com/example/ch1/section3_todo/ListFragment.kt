093bc89c46978ab124b78132b9e95f17224a97c0
kkangs <kkang104@daum.net>
2025-05-08 16:09:50 +0900

    .

--- AndroidLab2/ch1/src/main/java/com/example/ch1/section3_todo/ListFragment.kt
+++ AndroidLab2/ch1/src/main/java/com/example/ch1/section3_todo/ListFragment.kt
@@ -1,11 +1,15 @@
 package com.example.ch1.section3_todo
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ch1.databinding.FragmentListBinding
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ListFragment: Fragment() {
 
@@ -27,5 +31,35 @@ class ListFragment: Fragment() {
         binding.addBtn.setOnClickListener {
             (activity as Test3_3Activity).goAddFragment()//화면 전환...
         }
+
+        //초기 데이터 구축.. db select 해서..
+        val itemList = setRecyclerViewData(activity as Context)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.adapter = TodoAdapter(activity as Test3_3Activity, itemList)
+        binding.recyclerView.addItemDecoration(TodoDecoration(itemList))
+    }
+
+    fun setRecyclerViewData(context: Context): MutableList<Item>{
+        //db select 데이터..
+        val dbList = selectTodos(context)
+        //화면에 찍기 위한 데이터..
+        val itemList = mutableListOf<Item>()
+
+        var preDate: String? = null//이전 항목 데이터를 찍었던 날짜.. 만약 변경된거면 날짜 항목이 추가..
+        for(vo in dbList){
+            if(!vo.date.equals(preDate)){
+                //header 항목 데이터 추가..
+                val headerItem = HeaderItem(
+                    date = SimpleDateFormat("yyyy-MM-dd").format(Date(vo.date.toLong())))
+                itemList.add(headerItem)
+                preDate = vo.date
+            }
+            val completed = vo.completed != 0
+            val dataItem = DataItem(id = vo.id, title = vo.title, content = vo.content,
+                completed = completed)
+            itemList.add(dataItem)
+        }
+        return itemList
     }
 }
