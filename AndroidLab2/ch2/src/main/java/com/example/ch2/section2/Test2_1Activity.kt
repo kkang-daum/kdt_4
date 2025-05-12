7b4cfd1ecb7b72e7e25cd5b7fcd0e420483d40e7
kkangs <kkang104@daum.net>
2025-05-12 14:40:03 +0900

    .

--- AndroidLab2/ch2/src/main/java/com/example/ch2/section2/Test2_1Activity.kt
+++ AndroidLab2/ch2/src/main/java/com/example/ch2/section2/Test2_1Activity.kt
@@ -5,9 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback
 import com.example.ch2.R
 
-class Test2_1Activity : AppCompatActivity() {
+class Test2_1Activity : AppCompatActivity(), OnPreferenceStartFragmentCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +20,30 @@ class Test2_1Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main, MainSettingFragment())
+            .commit()
+
+    }
+
+    //fragment 중첩 구조에서.. <Preference> 에 의해 다른 Fragment 가 출력되어야 한다면..
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        val args = pref.extras
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment as String
+        )
+        fragment.arguments = args
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main, fragment)
+            .addToBackStack(null)
+            .commit()
+
+        return true
     }
 }
