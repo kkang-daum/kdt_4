76b52578d085ee381ecdfd1c3c100b6b929fedba
kkang <kkang104@gmail.com>
2025-05-14 09:42:21 +0900

    .

--- AndroidLab2/ch3/src/main/java/com/example/ch3/section3/Test3_1Activity.kt
+++ AndroidLab2/ch3/src/main/java/com/example/ch3/section3/Test3_1Activity.kt
@@ -1,5 +1,6 @@
 package com.example.ch3.section3
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -61,33 +62,59 @@ class Test3_1Activity : AppCompatActivity() {
 
 
             //방법 1 - thread...handler 구조................................
-            val handler = object: Handler(Looper.getMainLooper()){
-                //아래의 함수는 main thread 에 의해 호출된다..
-                //thread 가 sendMessage() 하는 순간..
-                //매개변수는 thread 에서 전달한 데이터..
-                override fun handleMessage(msg: Message) {
-                    if(msg.what == 1){
-                        Toast.makeText(this@Test3_1Activity, "${msg.obj as Long}", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
-            }
+//            val handler = object: Handler(Looper.getMainLooper()){
+//                //아래의 함수는 main thread 에 의해 호출된다..
+//                //thread 가 sendMessage() 하는 순간..
+//                //매개변수는 thread 에서 전달한 데이터..
+//                override fun handleMessage(msg: Message) {
+//                    if(msg.what == 1){
+//                        Toast.makeText(this@Test3_1Activity, "${msg.obj as Long}", Toast.LENGTH_SHORT)
+//                            .show()
+//                    }
+//                }
+//            }
+//
+//            val obj = object: Runnable{
+//                override fun run() {
+//                    var sum = 0L
+//                    for(i in 1..5_000_000_000){
+//                        sum += i
+//                    }
+//                    //데이터 발생.. 개발자 스레드에서 화면 변경 못한다.. main thread 에게 의뢰..
+//                    val message = Message()
+//                    message.what = 1//요청의 구분자..
+//                    message.obj = sum //넘기는 데이터..
+//                    handler.sendMessage(message)//요청 순간.. 이 순간.. main thread 동작..
+//                }
+//            }
+//            val thread = Thread(obj)
+//            thread.start()
 
-            val obj = object: Runnable{
-                override fun run() {
+            //방법2 - AsyncTask....
+            //Thread, Handler에 의해 처리되어야 할 내용을 AsyncTask 상속 클래스에 담아서 처리..
+            //제네픽 타입 : 백
+            // 그라운드 작업에 전달될 데이터 타입
+            // 스레드에 의해 반복적으로 발생되는 데이터 타입
+            // 스레드의 최종 결과 데이터 타입..
+            class MyAsyncTask: AsyncTask<Void, Long, Long>(){
+                //시간이 오래 걸리는 업무를 담는 함수.. 내부적으로 스레드에 의해 실행된다..
+                override fun doInBackground(vararg params: Void?): Long {
                     var sum = 0L
-                    for(i in 1..5_000_000_000){
+                    for(i in 1..1_000_000_000){
                         sum += i
                     }
-                    //데이터 발생.. 개발자 스레드에서 화면 변경 못한다.. main thread 에게 의뢰..
-                    val message = Message()
-                    message.what = 1//요청의 구분자..
-                    message.obj = sum //넘기는 데이터..
-                    handler.sendMessage(message)//요청 순간.. 이 순간.. main thread 동작..
+                    return sum//이곳에서 리턴 시킨 값이 이 스레드의 최종 결과 값이다..
+                    //이 값이 onPostExecute() 의 매개변수에 전달된다..
+                }
+
+                override fun onPostExecute(result: Long?) {
+                    //main thread 에 의해 실행..
+                    //UI
+                    Toast.makeText(this@Test3_1Activity, "$result", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
-            val thread = Thread(obj)
-            thread.start()
+            MyAsyncTask().execute()
         }
     }
 }
