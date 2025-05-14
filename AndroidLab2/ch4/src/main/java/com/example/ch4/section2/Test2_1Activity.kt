5bd1c2b4d8a8d03c1ff9423e412810b6e92ab62e
kkang <kkang104@gmail.com>
2025-05-14 15:11:48 +0900

    .

--- AndroidLab2/ch4/src/main/java/com/example/ch4/section2/Test2_1Activity.kt
+++ AndroidLab2/ch4/src/main/java/com/example/ch4/section2/Test2_1Activity.kt
@@ -1,5 +1,6 @@
 package com.example.ch4.section2
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.pm.PackageManager
 import android.os.Build
@@ -61,6 +62,27 @@ class Test2_1Activity : AppCompatActivity() {
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
         val builder: NotificationCompat.Builder
-        
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                "oneChannel",
+                "My Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            //만들어진 채널을 시스템에 등록..
+            manager.createNotificationChannel(channel)
+            //등록된 채널을 이용해 Builder 생성..
+            builder = NotificationCompat.Builder(this, "oneChannel")
+        }else {
+            builder = NotificationCompat.Builder(this)
+        }
+
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setContentTitle("메시지 도착")
+        builder.setContentText("안녕하세요")
+
+        //발생............
+        manager.notify(11, builder.build())
+
     }
 }
