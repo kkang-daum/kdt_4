a03f3aa6629d8488b1ee65d8aedc01cfcfacdf15
kkang <a@a.com>
2025-05-15 14:06:31 +0900

    .

--- AndroidLab2/ch5/src/main/java/com/example/ch5/section2/Test2_1Activity.kt
+++ AndroidLab2/ch5/src/main/java/com/example/ch5/section2/Test2_1Activity.kt
@@ -1,10 +1,14 @@
 package com.example.ch5.section2
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ch5.R
@@ -24,17 +28,46 @@ class Test2_1Activity : AppCompatActivity() {
             insets
         }
 
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ){
+            if(it){
+                Toast.makeText(this, "permission ok..", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "permission denied..", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         binding.backgroundBtn.setOnClickListener {
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                if(ContextCompat.checkSelfPermission(
+                        this,
+                        "android.permission.POST_NOTIFICATIONS"
+                    ) == PackageManager.PERMISSION_GRANTED){
+                    val serviceIntent = Intent(this, MyService2::class.java)
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                        startForegroundService(serviceIntent)
+                    }else {
+                        startService(serviceIntent)
+                    }
+                }else {
+                    permissionLauncher.launch("android.permission.POST_NOTIFICATIONS")
+                }
+            }else {
+                val serviceIntent = Intent(this, MyService2::class.java)
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    startForegroundService(serviceIntent)
+                }else {
+                    startService(serviceIntent)
+                }
+            }
+
             //서비스를 foreground service 로 구동...
             //startForegroundService() 로 실행시키면 서비스는 구동은 된다.. 백그라운드 상황에서도
             //얼마후... android.app.RemoteServiceException$ForegroundServiceDidNotStartInTimeException: Context.startForegroundService() did not then call Service.startForeground(): ServiceRecord{5ad0cfa u0 com.example.ch5/.section2.MyService2 c:com.example.ch5}
             //에러 발생..
-            val serviceIntent = Intent(this, MyService2::class.java)
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                startForegroundService(serviceIntent)
-            }else {
-                startService(serviceIntent)
-            }
+
         }
     }
 }
