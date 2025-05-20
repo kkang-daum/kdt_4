5335f9a40345d7f631b9c60b4f2bbd2ec4d192ea
kkang <a@a.com>
2025-05-20 11:28:42 +0900

    .

--- AndroidLab2/ch8/build.gradle.kts
+++ AndroidLab2/ch8/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
---
+++ AndroidLab2/ch8/src/main/java/com/example/ch8/section2/Test2_1Activity.kt
@@ -0,0 +1,21 @@
+package com.example.ch8.section2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch8.R
+
+class Test2_1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_test21)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
