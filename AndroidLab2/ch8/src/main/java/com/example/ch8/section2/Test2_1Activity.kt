c7443a7a8eb692e7f1223348e11668c237f5423d
kkang <a@a.com>
2025-05-20 11:50:30 +0900

    .

--- AndroidLab2/ch8/src/main/java/com/example/ch8/section2/Test2_1Activity.kt
+++ AndroidLab2/ch8/src/main/java/com/example/ch8/section2/Test2_1Activity.kt
@@ -6,16 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ch8.R
+import com.example.ch8.databinding.ActivityTest21Binding
 
 class Test2_1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_test21)
+
+        val binding = ActivityTest21Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        q : 검색어..
+        val url ="https://newsapi.org/v2/everything?q=travel&apiKey=&page=1&pageSize=5"
     }
 }
