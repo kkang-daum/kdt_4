7115e2c8fa582df2547b3f911c9989c1498e3503
kkang <a@a.com>
2025-05-27 11:21:49 +0900

    .

--- Android3/ch2/src/main/java/com/example/ch2/AuthActivity.kt
+++ Android3/ch2/src/main/java/com/example/ch2/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.example.ch2
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -35,7 +36,14 @@ class AuthActivity : AppCompatActivity() {
                 loginBtn.visibility = View.VISIBLE
             }
         }else if(mode == "signup"){
-            
+            binding.run {
+                logoutBtn.visibility = View.GONE
+                goSignupBtn.visibility = View.GONE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signupBtn.visibility = View.VISIBLE
+                loginBtn.visibility = View.GONE
+            }
         }
     }
 
@@ -50,6 +58,81 @@ class AuthActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setSupportActionBar(binding.toolbar)
+
+        //이 액티비티의 초기 상태..
+        if(MyApplication.checkAuth()){
+            changeVisibility("login")
+        }else {
+            changeVisibility("logout")
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            //로그아웃...
+            MyApplication.auth.signOut()
+            MyApplication.email = null
+            changeVisibility("logout")
+        }
+
+        binding.goSignupBtn.setOnClickListener {
+            changeVisibility("signup")//회원가입되게 화면 조정..
+        }
+
+        binding.signupBtn.setOnClickListener {
+            //회원가입....
+            //유저 입력 데이터 획득...
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+
+            //firebase 에.. 유저 입력 데이터 전달.. 회원가입.. 결과는 콜백으로..
+            MyApplication.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    //화면에 입력된 데이터 지우고..
+                    binding.authEmailEditView.text.clear()
+                    binding.authPasswordEditView.text.clear()
+                    if(task.isSuccessful){
+                        //서버 등록이 성공했다면..
+                        //검증 이메일을 발송....
+                        MyApplication.auth.currentUser?.sendEmailVerification()
+                            ?.addOnCompleteListener { sendTask ->
+                                if(sendTask.isSuccessful){
+                                    //검증 이메일 발송이 성공한 경우...
+                                    Toast.makeText(baseContext, "회원가입이 성공했습니다. 전송된 이메일을 확인해 주세요.",
+                                        Toast.LENGTH_SHORT).show()
+                                    changeVisibility("logout")
+                                }else {
+                                    Toast.makeText(baseContext, "메일 전송 실패", Toast.LENGTH_SHORT).show()
+                                    changeVisibility("logout")
+                                }
+                            }
+                    }else {
+                        Toast.makeText(baseContext, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                        changeVisibility("logout")
+                    }
+                }
+        }
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+            MyApplication.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) {task ->
+                    binding.authEmailEditView.text.clear()
+                    binding.authPasswordEditView.text.clear()
+                    if(task.isSuccessful){
+                        if(MyApplication.checkAuth()){
+                            MyApplication.email = email
+                            changeVisibility("login")
+                        }else {
+                            Toast.makeText(baseContext, "전송된 이메일 인증이 되지 않았습니다.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }else {
+                        Toast.makeText(baseContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
