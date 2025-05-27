b900cadf5aac728e3f26cf2a2f39f0280f9276e5
kkang <a@a.com>
2025-05-27 10:50:35 +0900

    .

--- Android3/ch2/src/main/java/com/example/ch2/AuthActivity.kt
+++ Android3/ch2/src/main/java/com/example/ch2/AuthActivity.kt
@@ -1,16 +1,51 @@
 package com.example.ch2
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ch2.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAuthBinding
+
+    //매개변수의 모드를 보고.. 화면에 뷰 출력 상태 조정하는 개발자 함수..
+    fun changeVisibility(mode: String){
+        if(mode == "login"){
+            binding.run {
+                authMainTextView.text = "${MyApplication.email} 님 반갑습니다."
+                logoutBtn.visibility = View.VISIBLE
+                goSignupBtn.visibility = View.GONE
+                authEmailEditView.visibility = View.GONE
+                authPasswordEditView.visibility = View.GONE
+                signupBtn.visibility = View.GONE
+                loginBtn.visibility = View.GONE
+            }
+        }else if(mode == "logout"){
+            binding.run {
+                authMainTextView.text = "로그인 하거나 회원가입 해주세요."
+                logoutBtn.visibility = View.GONE
+                goSignupBtn.visibility = View.VISIBLE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signupBtn.visibility = View.GONE
+                loginBtn.visibility = View.VISIBLE
+            }
+        }else if(mode == "signup"){
+            
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
