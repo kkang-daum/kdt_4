b829a677dda535be1f134da39490e25843323d76
kkang <a@a.com>
2025-05-27 10:33:47 +0900

    .

--- Android3/ch2/src/main/java/com/example/ch2/MainActivity.kt
+++ Android3/ch2/src/main/java/com/example/ch2/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.ch2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ch2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(!MyApplication.checkAuth()){
+            binding.logoutTextView.visibility = View.VISIBLE
+        }else {
+            binding.logoutTextView.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        startActivity(Intent(this, AuthActivity::class.java))
+        return super.onOptionsItemSelected(item)
     }
 }
