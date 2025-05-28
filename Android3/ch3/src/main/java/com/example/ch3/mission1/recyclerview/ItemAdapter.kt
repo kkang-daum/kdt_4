5d54238c7ab764dd07d04c00655b67042ac41e2d
kkang <a@a.com>
2025-05-28 15:39:31 +0900

    .

--- Android3/ch3/src/main/java/com/example/ch3/mission1/recyclerview/ItemAdapter.kt
+++ Android3/ch3/src/main/java/com/example/ch3/mission1/recyclerview/ItemAdapter.kt
@@ -3,8 +3,11 @@ package com.example.ch3.mission1.recyclerview
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.ch3.R
 import com.example.ch3.databinding.ItemMainBinding
 import com.example.ch3.mission1.model.Item
 
@@ -22,6 +25,25 @@ class ItemAdapter (val context: Context, val datas: List<Item>): RecyclerView.Ad
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = datas[position]
+
+        holder.binding.run {
+            itemTitle.text = item.title
+            itemDesc.text = item.description
+            itemTime.text = item.publishedAt
+
+            visit.setOnClickListener {
+                //WebFragment 실행..
+                (context as AppCompatActivity).supportFragmentManager.beginTransaction().run {
+                    replace(R.id.main, WebFragment(item.url))
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+            Glide.with(context)
+                .load(item.urlToImage)
+                .into(itemImage)
+        }
+
     }
 
 
