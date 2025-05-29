7b6d79c029c4105995486a31cf4e13eddbb7cb1b
kkang <a@a.com>
2025-05-29 11:31:07 +0900

    .

--- Android3/ch3/src/main/java/com/example/ch3/section4/Test4_1Activity.kt
+++ Android3/ch3/src/main/java/com/example/ch3/section4/Test4_1Activity.kt
@@ -5,17 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.example.ch3.R
+import com.example.ch3.databinding.ActivityTest41Binding
 
 class Test4_1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_test41)
+
+        val binding = ActivityTest41Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //case2 - code 에서 graph xml 지정..
+        val navHost = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, navHost)
+            .setPrimaryNavigationFragment(navHost)
+            .commit()
     }
 }
