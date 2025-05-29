ffdc7265e978f38257034a9660e36569e65e073f
kkang <a@a.com>
2025-05-29 13:16:39 +0900

    .

--- Android3/ch3/src/main/java/com/example/ch3/section4/Test4_1Activity.kt
+++ Android3/ch3/src/main/java/com/example/ch3/section4/Test4_1Activity.kt
@@ -1,11 +1,15 @@
 package com.example.ch3.section4
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.ch3.R
 import com.example.ch3.databinding.ActivityTest41Binding
 
@@ -23,11 +27,23 @@ class Test4_1Activity : AppCompatActivity() {
             insets
         }
 
+        setSupportActionBar(binding.toolbar)
+
         //case2 - code 에서 graph xml 지정..
         val navHost = NavHostFragment.create(R.navigation.nav_graph)
         supportFragmentManager.beginTransaction()
             .replace(R.id.nav_host_fragment, navHost)
             .setPrimaryNavigationFragment(navHost)
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_nav, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(this, R.id.nav_host_fragment))
+        return super.onOptionsItemSelected(item)
     }
 }
