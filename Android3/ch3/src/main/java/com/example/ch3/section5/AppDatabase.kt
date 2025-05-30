392cbf38294b89077b064daf75e8cf481668cbd2
kkang <a@a.com>
2025-05-30 09:51:07 +0900

    .

--- Android3/ch3/src/main/java/com/example/ch3/section5/AppDatabase.kt
+++ Android3/ch3/src/main/java/com/example/ch3/section5/AppDatabase.kt
@@ -2,10 +2,12 @@ package com.example.ch3.section5
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 //Entity, Dao 등은 필요에 의해 여러개 선언...
 //Database 클래스 선언은 한번만..
-@Database(entities = arrayOf(User::class), version = 1)
+@Database(entities = arrayOf(User::class, User2::class, Address::class), version = 1)
+@TypeConverters(Converter::class)
 abstract class AppDatabase: RoomDatabase() {
     //dao 를 획득할 함수..
     abstract fun userDao(): UserDAO
