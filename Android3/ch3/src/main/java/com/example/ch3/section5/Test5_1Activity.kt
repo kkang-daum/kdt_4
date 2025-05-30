9099ee775b9912f99c0afb612eabdd1a216a8dbc
kkang <a@a.com>
2025-05-30 10:06:47 +0900

    .

--- Android3/ch3/src/main/java/com/example/ch3/section5/Test5_1Activity.kt
+++ Android3/ch3/src/main/java/com/example/ch3/section5/Test5_1Activity.kt
@@ -11,6 +11,7 @@ import com.example.ch3.databinding.ActivityTest51Binding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import java.util.Date
 import kotlin.coroutines.CoroutineContext
 
 class Test5_1Activity : AppCompatActivity(), CoroutineScope {
@@ -53,20 +54,33 @@ class Test5_1Activity : AppCompatActivity(), CoroutineScope {
         binding.button.setOnClickListener {
             //id 를 모두 0으로 지정한 이유는 Entity 클래스내에 id 가 primary key 로 선언되어
             //있고 auto generate 로 선언되어 있어서.. insert 시에 자동 값 증가되기 때문에..
-            val user1 = User(0, "gildong", "hong")
-            val user2 = User(0, "gildong2", "hong2")
-            val user3 = User(0, "gildong3", "hong3")
+//            val user1 = User(0, "gildong", "hong")
+//            val user2 = User(0, "gildong2", "hong2")
+//            val user3 = User(0, "gildong3", "hong3")
+//
+//            dao.insertUser(user1)
+//            dao.insertUser(user2)
+//            dao.insertUser(user3)
+//
+//            var resultTxt = ""
+//
+//            dao.getAll().forEach {
+//                resultTxt += "$it \n"
+//            }
+//            binding.resultView.text = resultTxt
 
-            dao.insertUser(user1)
-            dao.insertUser(user2)
-            dao.insertUser(user3)
+            val list = mutableListOf("hello", "world")
+            val user = User2(id = 0, firstName = "kim", lastName = "lee",
+                address = Address(id = 0, street = "a", state = "a", city = "a"),
+                datas = list, regDate = Date())
 
-            var resultTxt = ""
-
-            dao.getAll().forEach {
-                resultTxt += "$it \n"
+            dao.insertUser2(user)
+            var txt = ""
+            dao.getAll2().forEach {
+                txt += "$it \n"
             }
-            binding.resultView.text = resultTxt
+            binding.resultView.text = txt
+
         }
     }
 }
