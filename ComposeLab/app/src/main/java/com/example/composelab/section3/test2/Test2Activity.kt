84d93168a30c20e60611185072c50b9b6aa473ee
kkang <a@a.com>
2025-06-02 11:14:59 +0900

    .

--- ComposeLab/app/src/main/java/com/example/composelab/section3/test2/Test2Activity.kt
+++ ComposeLab/app/src/main/java/com/example/composelab/section3/test2/Test2Activity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,17 +61,49 @@ fun Greeting4(name: String, modifier: Modifier = Modifier) {
 //        }
 
         //Arrangement(주축), Alignment(반대축)
+//        Row(modifier = Modifier
+//            .background(Color.Yellow)
+//            .fillMaxWidth()
+//            .height(300.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.Center
+//        ) {
+//            MyText(data = "A")
+//            MyText(data = "B")
+//            MyText(data = "C")
+//        }
+
+        //SpaceXXX
         Row(modifier = Modifier
             .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(300.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             MyText(data = "A")
             MyText(data = "B")
             MyText(data = "C")
         }
+        Spacer(modifier = Modifier.size(width = 0.dp, height = 10.dp))
+        Row(modifier = Modifier
+            .background(Color.Yellow)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            MyText(data = "A")
+            MyText(data = "B")
+            MyText(data = "C")
+        }
+        Spacer(modifier = Modifier.size(width = 0.dp, height = 10.dp))
+        Row(modifier = Modifier
+            .background(Color.Yellow)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            MyText(data = "A")
+            MyText(data = "B")
+            MyText(data = "C")
+        }
+        Spacer(modifier = Modifier.size(width = 0.dp, height = 10.dp))
     }
 }
 
