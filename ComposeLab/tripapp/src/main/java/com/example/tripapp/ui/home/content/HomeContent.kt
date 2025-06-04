aa45bd7cdd0fb84af418858ce562707d5e5b741f
kkang <a@a.com>
2025-06-04 11:49:11 +0900

    .

--- ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/home/content/HomeContent.kt
+++ ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/home/content/HomeContent.kt
@@ -1,10 +1,17 @@
 package com.example.tripapp.ui.home.content
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tripapp.R
@@ -58,7 +66,29 @@ fun HomeContent(
             }
         }
         item {
-
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("특가 여행지")
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF3899DD)
+                    )
+                ) {
+                    Row {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowDropDown,
+                            contentDescription = ""
+                        )
+                        Text("전체 여행지 보기")
+                    }
+                }
+            }
         }
         items(){
 
