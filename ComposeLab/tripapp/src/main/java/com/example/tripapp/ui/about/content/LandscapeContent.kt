cdab34cf889a1fbccdfc6296d867b22dccd58ef2
kkang <a@a.com>
2025-06-04 15:02:03 +0900

    .

--- ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/about/content/LandscapeContent.kt
+++ ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/about/content/LandscapeContent.kt
@@ -7,13 +7,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tripapp.R
 
 @Composable
@@ -35,9 +42,35 @@ fun LandscapeContent(
         )
 
         Column(
-            modifier = 
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
         ) {
-
+            Text(
+                text = stringResource(R.string.about_main),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                ),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
+            Text(
+                text = stringResource(R.string.about_detail),
+                style = TextStyle(
+                    fontSize = 14.sp
+                ),
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text(stringResource(R.string.about_more))
+            }
         }
     }
 }
