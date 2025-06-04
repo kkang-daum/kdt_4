2eccfb1e28a5e26ce7f9a810a28ef85e16bc53e9
kkang <a@a.com>
2025-06-04 14:47:20 +0900

    .

--- ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/about/content/PortraitContent.kt
+++ ComposeLab/tripapp/src/main/java/com/example/tripapp/ui/about/content/PortraitContent.kt
@@ -1,4 +1,65 @@
 package com.example.tripapp.ui.about.content
 
-class PortraitContent {
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tripapp.R
+
+//세로방향 화면....
+@Composable
+fun PortraitContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.about_main),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            //리소스를 이용함으로.. values 혹은 values-ko-Rkr 중 어디를 이용할지 스스로 결정
+            text = stringResource(R.string.about_main),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        )
+        Text(
+            text = stringResource(R.string.about_detail),
+            style = TextStyle(
+                fontSize = 14.sp
+            ),
+            modifier = Modifier
+                .padding(top = 20.dp)
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(stringResource(R.string.about_more))
+        }
+    }
 }
