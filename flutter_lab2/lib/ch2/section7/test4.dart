3aaeecc978d5849882580e167224c78baf240012
kkang <kkang104@gmail.com>
2025-06-11 14:07:32 +0900

    .

--- flutter_lab2/lib/ch2/section7/test4.dart
+++ flutter_lab2/lib/ch2/section7/test4.dart
@@ -1,10 +1,17 @@
 class MyClass {
-  int data;
-  MyClass(this.data);
+  final int data;
+  const MyClass(this.data);
 }
 
 main(){
-  var obj1 = MyClass(10);
-  var obj2 = MyClass(10);
-  print()
+  var obj1 = const MyClass(10);
+  var obj2 = const MyClass(10);
+  print("${obj1 == obj2}");//true
+
+  var obj3 = MyClass(10);
+  var obj4 = MyClass(10);
+  print("${obj3 == obj4}");//false
+
+  var obj5 = const MyClass(20);
+  print("${obj1 == obj5}");//false
 }
