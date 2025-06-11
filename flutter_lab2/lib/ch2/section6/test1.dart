9612e77cb0dd8937eaa9cd59ef41cefc5dc08c3f
kkang <kkang104@gmail.com>
2025-06-11 10:17:35 +0900

    .

--- flutter_lab2/lib/ch2/section6/test1.dart
+++ flutter_lab2/lib/ch2/section6/test1.dart
@@ -6,6 +6,30 @@ class User {
   }
 }
 
+//casecade vs 메서드 체이닝...
+class StringBuilder {
+  String _content = "";
+
+  StringBuilder append(String text){
+    _content += text;
+    return this;
+  }
+  void clear(){
+    _content = "";
+  }
+  String build() => _content;
+}
+
+//중첩 구조로 casecade 이용..
+class Address {
+  String? street;
+  String? city;
+}
+class Person {
+  String? name;
+  Address address = Address();
+}
+
 main(){
   int a = 8;
   print('${a / 5}');//1.6
@@ -18,4 +42,46 @@ main(){
     obj.some();//ok...
   }
   (obj as User).some();
+
+  //casecade 연산자................
+  var user = User();
+  user.name = "kim";
+  user.age = 20;
+  user.some();
+
+  User()
+    ..name = "lee"
+    ..age = 30
+    ..some();
+
+  var list = []
+    ..add(10)
+    ..add(20)
+    ..add(30);
+
+
+  //casecade vs 메서드 체이닝...
+  //메서드 체이닝으로 사용하고자 하는 메서드는 리턴 타입이 자기 자신만 가능..
+  var result = StringBuilder()
+    .append("Hello")
+    .append(" ")
+    .append("World")
+    // .clear()//error...
+    .build();
+
+  print(result);
+
+  var builder = StringBuilder()
+    ..append("Hello")
+    ..append(" ")
+    ..append("World")
+    ..clear();
+
+  //중첩 casecase
+  var person = Person()
+    ..name = "Kim"
+    ..address = (Address()
+      ..street = "a street"
+      ..city = "a city"
+    );
 }
