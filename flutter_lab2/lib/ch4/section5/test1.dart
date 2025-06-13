a4090d54db6a61ef7babfd23dfdd34f90885e1b1
kkang <kkang104@gmail.com>
2025-06-13 13:11:08 +0900

    .

---
+++ flutter_lab2/lib/ch4/section5/test1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lab2/ch4/section5/widgets/header_widget.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('layout test'),),
+        body: Column(
+          children: [
+            HeaderWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_lab2/lib/ch4/section5/widgets/header_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const List<String> choices = [
+  "신고", "공유하기", "링크 복사"
+];
+
+class HeaderWidget extends StatelessWidget {
+  void select(String choice){
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset('images/lab_instagram_icon_0.jpg'),
+        Text('instagram'),
+        PopupMenuButton<String>(
+            //... 메뉴 클릭시의 이벤트 처리..
+            onSelected: select,
+            //... 클릭시 확장되는 부분을 구성하기 위해서 호출..
+            itemBuilder: (BuildContext context){
+              //이 함수에서 리턴시킨 부분이 확장되서 나온다..
+              return choices.map<PopupMenuItem<String>>((String choice){
+                return PopupMenuItem<String>(
+                  value: choice,//메뉴 클릭시에.. onSelected 이벤트 콜백에 전달할 데이터
+                  child: Text(choice),//각 메뉴 구성
+                );
+              }).toList();
+            }
+        ),
+      ],
+    );
+  }
+}
