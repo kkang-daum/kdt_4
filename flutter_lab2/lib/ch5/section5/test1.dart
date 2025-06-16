1d9bc12e0e84ec6b38d7bf366bbcc6c13bf85672
kkang <kkang104@gmail.com>
2025-06-16 15:11:34 +0900

    .

--- flutter_lab2/lib/ch5/section5/test1.dart
+++ flutter_lab2/lib/ch5/section5/test1.dart
@@ -58,7 +58,53 @@ class MyAppState extends State<MyApp>{
         ),
         body: widgets.elementAt(selectedItem),
         bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.shifting,//fixed, shifting(
+          type: BottomNavigationBarType.shifting,//fixed(고정), shifting(애니메이션)
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'One',
+              backgroundColor: Colors.green,//이 item 이 선택될 때, navigation bar 전체 색상
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Two',
+              backgroundColor: Colors.red,//이 item 이 선택될 때, navigation bar 전체 색상
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Three',
+              backgroundColor: Colors.purple,//이 item 이 선택될 때, navigation bar 전체 색상
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Four',
+              backgroundColor: Colors.pink,//이 item 이 선택될 때, navigation bar 전체 색상
+            ),
+          ],
+          currentIndex: selectedItem,
+          selectedItemColor: Colors.amber,//선택된 아이템을 표시할 색상.. 아이콘의 색상..
+          onTap: onItemTapped,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                child: Text('Drawer header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue
+                ),
+              ),
+              ListTile(
+                title: Text('Item1'),
+                onTap: (){},
+              ),
+              ListTile(
+                title: Text('Item2'),
+                onTap: (){},
+              )
+            ],
+          ),
         ),
       ),
     );
