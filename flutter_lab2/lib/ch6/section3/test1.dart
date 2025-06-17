510f4cb3b03d4812c0bb8f8313dd0145227b51e8
kkang <kkang104@gmail.com>
2025-06-17 15:50:19 +0900

    .

---
+++ flutter_lab2/lib/ch6/section3/test1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  //리턴 타입 Future...
+  Future<int> funA() {
+    return Future.delayed(Duration(seconds: 3), (){
+      return 10;
+    });
+  }
+  Future<int> funB(int arg){
+    return Future.delayed(Duration(seconds: 2), (){
+      return arg * arg;
+    });
+  }
+  Future<int> calFun() async {
+    // funA().then((value){
+    //
+    // });
+    int aData = await funA();
+    int bData = await funB(aData);
+
+    return bData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Future Test'),),
+        body: Center(
+          child: FutureBuilder(
+            future: calFun(),
+          ),
+        ),
+      ),
+    );
+  }
+}
