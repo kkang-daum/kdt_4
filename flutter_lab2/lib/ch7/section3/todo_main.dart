0241e6e5ab104fed4b12b3ce17c758d9905dee94
kkang <kkang104@gmail.com>
2025-06-19 07:58:12 +0900

    .

--- flutter_lab2/lib/ch7/section3/todo_main.dart
+++ flutter_lab2/lib/ch7/section3/todo_main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab2/ch3/section3/test2.dart';
 import 'package:flutter_lab2/ch7/section3/providers/todo_provider.dart';
 import 'package:provider/provider.dart';
+import 'screen/home_screen.dart';
 
 main() => runApp(MyApp());
 
