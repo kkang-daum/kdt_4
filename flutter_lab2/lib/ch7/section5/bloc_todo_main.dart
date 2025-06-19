2b640371b40899b9fe285e596f04daaea5364595
kkang <kkang104@gmail.com>
2025-06-19 11:50:42 +0900

    .

--- flutter_lab2/lib/ch7/section5/bloc_todo_main.dart
+++ flutter_lab2/lib/ch7/section5/bloc_todo_main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_lab2/ch7/section5/bloc/todo_bloc.dart';
+import 'screens/home_screen.dart';
 
 
 void main() => runApp(TodosApp());
@@ -7,6 +10,11 @@ class TodosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //add.................................
-    
+    return MaterialApp(
+      home: BlocProvider<TodoBloc>(
+        create: (context) => TodoBloc(),
+        child: HomeScreen(),
+      ),
+    );
   }
 }
