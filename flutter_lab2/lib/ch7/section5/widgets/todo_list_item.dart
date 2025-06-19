77e995ff485194705208e5aaea9388193b248837
kkang <kkang104@gmail.com>
2025-06-19 11:33:37 +0900

    .

--- flutter_lab2/lib/ch7/section5/widgets/todo_list_item.dart
+++ flutter_lab2/lib/ch7/section5/widgets/todo_list_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_lab2/ch7/section5/bloc/todo_bloc.dart';
+import 'package:flutter_lab2/ch7/section5/event/todo_event.dart';
+import '../state/todo_state.dart';
 
 class TodoListItem extends StatelessWidget {
   final Todo todo;
@@ -9,6 +12,22 @@ class TodoListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //add.....................
-    
+    TodoBloc bloc = BlocProvider.of<TodoBloc>(context);
+    return ListTile(
+      leading: Checkbox(
+        value: todo.completed,
+        onChanged: (bool? checked) {
+          //bloc 에 이벤트 발생해서 앱 전역에 데이터 유지되게..
+          bloc.add(ToggleCompletedTodoEvent(todo));
+        },
+      ),
+      title: Text(todo.title),
+      trailing: IconButton(
+        onPressed: () {
+          bloc.add(DeleteTodoEvent(todo));
+        },
+        icon: Icon(Icons.delete, color: Colors.red),
+      ),
+    );
   }
 }
