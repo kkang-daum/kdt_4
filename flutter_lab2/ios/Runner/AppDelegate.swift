b20b388997d7a9de02cc58c21626a1ad82642ca0
kkang <kkang104@gmail.com>
2025-06-19 13:50:41 +0900

    .

--- flutter_lab2/ios/Runner/AppDelegate.swift
+++ flutter_lab2/ios/Runner/AppDelegate.swift
@@ -9,5 +9,7 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+
   }
 }
---
+++ flutter_lab2/lib/ch8/section1/test1.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
+  String? resultMessage;//native 에 일시키고.. 넘어온 결과 데이터..
+  String? receiveMessage;//native 에서 먼저 채널을 통해 전달한 데이터..
+
+  Future<Null> messageTest() async {
+    //메시지 채널 준비.. native 와 동일이름으로 준비해야 한다..
+    //원한다면 이름을 다르게 해서 하나의 앱에서 여러 채널을 이용해도 된다.
+    var channel = BasicMessageChannel("myMessageChannel", StringCodec());
+    //native 에 문자열 데이터를 전달하면서 일 시키기....
+    String? result = await channel.send("Hello from Dart");
+    setState(() {
+      resultMessage = result;
+    });
+
+    //dart 는 가만히 있는데.. native 에서 먼저 채널로 데이터를 전달하는 경우..
+    //콜백의 매개변수는 네이티브가 전달한 데이터..
+    channel.setMessageHandler((String? message) async {
+      setState(() {
+        receiveMessage = message;
+      });
+      return "reply from dart";
+    });
+  }
+}
