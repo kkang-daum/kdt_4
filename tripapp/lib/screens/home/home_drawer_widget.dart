0c20462219f6484455744ee64701401d60b04437
kkang <kkang104@gmail.com>
2025-06-20 11:50:08 +0900

    .'

---
+++ tripapp/lib/screens/home/home_drawer_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeDrawer extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          //drawer 상단을 사용자 정보로 많이 꾸미더라..
+          //그 부분을 도와주기 위해서..
+          UserAccountsDrawerHeader(
+              accountName: Text("홍길동"),
+              accountEmail: Text("user@example.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/user_basic.jpg"),
+              ),
+              decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text("About"),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("MyInfo"),
+            onTap: (){},
+          )
+        ],
+      ),
+    );
+  }
+}
