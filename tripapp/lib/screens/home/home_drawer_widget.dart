dfdd90d15a42362f6db3ff42babbea65b62ce406
kkang <kkang104@gmail.com>
2025-06-20 16:25:15 +0900

    .'

--- tripapp/lib/screens/home/home_drawer_widget.dart
+++ tripapp/lib/screens/home/home_drawer_widget.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tripapp/providers/user_provider.dart';
 import 'package:tripapp/routes/app_routes.dart';
 
 class HomeDrawer extends StatelessWidget{
@@ -8,15 +12,21 @@ class HomeDrawer extends StatelessWidget{
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          //drawer 상단을 사용자 정보로 많이 꾸미더라..
-          //그 부분을 도와주기 위해서..
-          UserAccountsDrawerHeader(
-              accountName: Text("홍길동"),
-              accountEmail: Text("user@example.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage("assets/images/user_basic.jpg"),
-              ),
-              decoration: BoxDecoration(color: Colors.blue),
+          Consumer<UserProvider>(
+            builder: (context, userProvider, child){
+              //drawer 상단을 사용자 정보로 많이 꾸미더라..
+              //그 부분을 도와주기 위해서..
+              return UserAccountsDrawerHeader(
+                accountName: Text(userProvider.userInfo?.name ?? "홍길동"),
+                accountEmail: Text(userProvider.userInfo?.email ?? "user@example.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: userProvider.userInfo?.profileImagePath != null
+                    ? FileImage(File(userProvider.userInfo!.profileImagePath!),)
+                      : AssetImage("assets/images/user_basic.jpg"),
+                ),
+                decoration: BoxDecoration(color: Colors.blue),
+              );
+            },
           ),
           ListTile(
             leading: Icon(Icons.info),
