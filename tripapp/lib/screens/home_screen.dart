2f8f283da7e10d0ac47f377dcc23880c48b59f7c
kkang <kkang104@gmail.com>
2025-06-20 11:51:01 +0900

    .'

--- tripapp/lib/screens/home_screen.dart
+++ tripapp/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tripapp/providers/trip_provider.dart';
+import 'package:tripapp/screens/home/home_drawer_widget.dart';
 import 'package:tripapp/screens/home/home_grid_widget.dart';
 import 'package:tripapp/screens/home/home_middle_widget.dart';
 import 'package:tripapp/screens/home/home_top_widget.dart';
@@ -19,6 +20,7 @@ class HomeScreen extends StatelessWidget {
         foregroundColor: Colors.black,
         elevation: 1,
       ),
+      drawer: HomeDrawer(),
       body: Column(
         children: [
           HomeTopWidget(),
