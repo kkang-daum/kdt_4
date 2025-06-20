ebefc8e4aafb9dbe134ccd8e9b712c409069e14f
kkang <kkang104@gmail.com>
2025-06-20 13:21:17 +0900

    .'

---
+++ tripapp/lib/screens/myinfo_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:tripapp/routes/app_routes.dart';
+import 'package:tripapp/screens/myinfo/myinfo_empty_state_widget.dart';
+
+
+//사용자 설정 정보가 있는지에 따라.. 화면에 출력되는 위젯을 다르게..
+//상태가 유지되어야 하는데.. 앱 전역.. 여러 위젯간의 상태는 아니다..
+class MyInfoScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyInfoScreenState();
+  }
+}
+
+class MyInfoScreenState extends State<MyInfoScreen>{
+  bool _showForm = false;//화면에 출력되는 위젯을 조정하기 위한 상태..
+
+  void handleShowForm(bool shouldShow){
+    
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyInfo"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: (){
+              Navigator.pushNamedAndRemoveUntil(
+                //home 에서 뿐만이 아니라.. 다른 화면에서 사용자 정보를 위해 들어 올수도 있다는가정
+                //화면 stack 을 모두 지우고 home 으로 이동..
+                context, 
+                AppRoutes.home,
+                  (route) => false
+              );
+            },
+          )
+        ],
+      ),
+      body: MyInfoEmptyStateWidget(handleShowForm),
+    );
+  }
+}
