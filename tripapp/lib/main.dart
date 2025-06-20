ceee8c218c68d4e48f1c6785815efa0074a0a3ed
kkang <kkang104@gmail.com>
2025-06-20 11:15:45 +0900

    .'

--- tripapp/lib/main.dart
+++ tripapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tripapp/providers/trip_provider.dart';
 import 'package:tripapp/routes/app_routes.dart';
 import 'package:tripapp/screens/home_screen.dart';
 
@@ -9,17 +11,23 @@ main() {
 class TripApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Trip App",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => TripProvider()),
+        ],
+      child: MaterialApp(
+        title: "Trip App",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: AppRoutes.home,
+        routes: {
+          AppRoutes.home: (context) => HomeScreen(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.home,
-      routes: {
-        AppRoutes.home: (context) => HomeScreen(),
-      },
     );
+
   }
 }
---
+++ tripapp/lib/models/trip_destination.dart
@@ -0,0 +1,17 @@
+class TripDestination {
+  int id;
+  String name;
+  String country;
+  String description;
+  String imagePath;
+  String discount;
+
+  TripDestination({
+    required this.id,
+    required this.name,
+    required this.country,
+    required this.description,
+    required this.imagePath,
+    required this.discount
+  });
+}
