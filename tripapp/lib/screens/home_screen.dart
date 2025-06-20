23e452b3ede15726b3b72ddaea08c67652a96dc0
kkang <kkang104@gmail.com>
2025-06-20 10:29:52 +0900

    .'

--- tripapp/lib/screens/home_screen.dart
+++ tripapp/lib/screens/home_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tripapp/screens/home/home_top_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("home screen");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trip App"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search),),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert,),),
+        ],
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
+      ),
+      body: Column(
+        children: [
+          HomeTopWidget(),
+        ],
+      ),
+    );
   }
 }
